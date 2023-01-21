fe1c321fa78251a4240fe6414c9cb91bfbced046
saewookkang <pjy9665@kakao.com>
2023-01-21 14:51:34 +0900

    keyof

---
+++ typescript/keyof.ts
@@ -0,0 +1,15 @@
+type Person = {
+  name: string;
+  age: number;
+}
+
+type Person2 = keyof Person; // same 'name' | 'age'
+
+const foo = (parameter: Person2) => {}
+
+foo('age') // ok
+foo('name') // ok
+foo(123) // error
+
+// Reference
+// https://www.typescriptlang.org/docs/handbook/2/keyof-types.html#handbook-content
